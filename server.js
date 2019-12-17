5e60e51b4bc8ae0cba6501f3ecdabcb14f03796d
Wyatt Phillips <datscigye@gmail.com>
2019-12-16 23:38:54 -0500

    Initialize routes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/posts', require('./routes/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
