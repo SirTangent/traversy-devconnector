9213f7cd7d82e94e03dc94f58e641e8a5ef0d529
Wyatt Phillips <datscigye@gmail.com>
2019-12-17 00:18:43 -0500

    Add registration request

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/profile', require('./routes/profile'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/posts', require('./routes/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
