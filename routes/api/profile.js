76e32a7a492a5e8eb67fc7757f7b4f145efc1dae
Wyatt Phillips <datscigye@gmail.com>
2019-12-18 18:18:11 -0500

    Add get user by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -119,4 +119,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json( {errors: [{msg: 'User does not have public profile'}] });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json( {errors: [{msg: 'User does not exist'}] });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
