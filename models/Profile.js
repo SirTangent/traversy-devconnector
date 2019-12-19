4b053dc3481fabbcfd59e4a01c38f65150e213e7
Wyatt Phillips <datscigye@gmail.com>
2019-12-19 13:58:15 -0500

    Add education CRUD routes

--- models/Profile.js
+++ models/Profile.js
@@ -54,6 +54,39 @@ const ProfileSchema = new mongoose.Schema({
             }
         }
     ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            location: {
+                type: String
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            },
+        }
+    ],
     social: {
         youtube: {
             type: String
