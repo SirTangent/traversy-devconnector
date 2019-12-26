46a30df94d6bf09d8b9d4e5ed9185640da0822e0
Wyatt Phillips <datscigye@gmail.com>
2019-12-26 12:15:11 -0500

    Init dashboard content and clear profile at logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ import { logout } from '../../actions/auth';
                  <Link to="/dashboard">Dashboard</Link>
              </li>
              <li>
-                 <a onClick={logout} href='/'>
+                 <a onClick={logout} href='#'>
                      <i className="fas fa-sign-out-alt"></i>{' '}
                      <span className="hide-sm">Logout</span>
                  </a>
