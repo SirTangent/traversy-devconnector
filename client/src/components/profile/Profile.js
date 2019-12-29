9a039c8512094326617ca37f5d4c4b1efd2fec08
Wyatt Phillips <datscigye@gmail.com>
2019-12-29 00:01:16 -0500

    Add profile top and about

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import Spinner from '../layout/Spinner';
 import { getProfileByID } from "../../actions/profile";
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({getProfileByID, profile: {profile, loading}, auth, match}) => {
     useEffect(() => {
@@ -17,6 +19,10 @@ const Profile = ({getProfileByID, profile: {profile, loading}, auth, match}) => 
                     Back to Profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
+                <div className="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
+                </div>
             </Fragment>}
         </Fragment>
     )
