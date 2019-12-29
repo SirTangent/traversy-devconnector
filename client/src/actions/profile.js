fb68f35882882acb63baeb958e2dc3d1cb96fe67
Wyatt Phillips <datscigye@gmail.com>
2019-12-28 22:14:15 -0500

    Start profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,14 +48,13 @@ export const getProfiles = () => async dispatch => {
 };
 
 // Get profile by ID
-export const getProfilesByID = userId => async dispatch => {
+export const getProfileByID = userId => async dispatch => {
     try {
         const res = await axios.get(`/api/profile/user/${userId}`);
-
         dispatch({
             type: GET_PROFILE,
             payload: res.data
-        })
+        });
     } catch (err) {
         dispatch({
             type: PROFILE_ERROR,
