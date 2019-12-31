82880ba967037fbb35e48636eb14992ca45cc243
Wyatt Phillips <datscigye@gmail.com>
2019-12-30 22:43:39 -0500

    Add comment delete action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     GET_POSTS,
     GET_POST,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from "./types";
 
 // Get posts
@@ -63,7 +65,7 @@ export const removeLike = postId => async dispatch => {
 // Delete post
 export const deletePost = postId => async dispatch => {
     try {
-        const res = await axios.delete(`/api/posts/${postId}`);
+        await axios.delete(`/api/posts/${postId}`);
 
         dispatch({
             type: DELETE_POST,
@@ -113,6 +115,50 @@ export const getPost = (postId) => async dispatch => {
             type: GET_POST,
             payload: res.data
         })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Removed', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
