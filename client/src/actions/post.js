384e84e154f85544325e28210b5b82a7c73d0bce
Wyatt Phillips <datscigye@gmail.com>
2019-12-30 20:11:30 -0500

    Add add post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {setAlert} from "./alert";
 import {
+    ADD_POST,
     DELETE_POST,
     GET_POSTS,
     POST_ERROR,
@@ -66,6 +67,31 @@ export const deletePost = postId => async dispatch => {
         dispatch({
             type: DELETE_POST,
             payload: postId
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post('/api/posts', formData, config);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
         });
 
         dispatch(setAlert('Post Removed', 'success'));
